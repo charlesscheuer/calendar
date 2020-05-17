6aff189ec1b579eaa8b88a1a3576b86867a0b27e
charlesscheuer <cscheuer@wustl.edu>
2020-05-17 15:10:28 -0400

    need to commit

---
+++ dist/precache-manifest.7380bc65b8dd78b7ecf49aa256b9a5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d416f178645e994c2981585f2198a7ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c8436b51f0ff77b62d",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "95810fb2f4890b6ebfe6",
+    "url": "/static/js/2.ad5ea5b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad5ea5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c8436b51f0ff77b62d",
+    "url": "/static/js/main.4fe52d9e.chunk.js"
+  },
+  {
+    "revision": "74e1cbaef1fda168aa72",
+    "url": "/static/js/runtime-main.1e79158c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/Popup.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>IT works!</h1>, document.getElementByID("root"));
