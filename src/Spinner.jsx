743f2818384448f3901eb372834008da3c5c3823
charlesscheuer <cscheuer@wustl.edu>
2020-06-11 00:16:56 -0400

    change spinner color

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Loader from 'react-loader-spinner'
+import Loader from "react-loader-spinner";
 
 // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 export default function Spinner(props) {
-
-  return(
+  return (
     <div className="Spinner">
       <Loader
         type="Rings"
-        color="#00BFFF"
+        color="#0c334d"
         height={50}
         width={50}
         timeout={10000} //10 secs
-     />
+      />
     </div>
   );
 }
